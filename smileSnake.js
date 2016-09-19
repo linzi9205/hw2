b487f844cfcda3a4fab548592ba008255a2945cc
linzi9205 <lwu@cca.edu>
2016-09-19 10:54:45 -0700

    Update smileSnake.js

--- smileSnake.js
+++ smileSnake.js
@@ -4,12 +4,15 @@ function setup() {
 }
 
 function draw(){
+  if (mouseX==0 && mouseY==0) {
+    return;
+  } 
   fill(255);
   ellipse(mouseX,mouseY,80,60);
   ellipse(mouseX-20,mouseY-8,16,10);
   ellipse(mouseX+20,mouseY-8,16,10);
   noFill();
   //ellipse(100,105,40,30);
-  arc(mouseX, mouseY+5, 40, 30, 0, PI, PI+QUARTER_PI);
-
+  arc(mouseX, mouseY+5, 40, 30, 0, PI);
+  
 }
